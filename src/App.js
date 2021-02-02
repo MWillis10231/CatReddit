4d52ee7b02e2949b6fac65f85519c05e1d046f34
Matrim <61281235+MWillis10231@users.noreply.github.com>
2021-02-02 12:20:26 +0100

    basic html outline

--- src/App.js
+++ src/App.js
@@ -6,51 +6,54 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+      <header>
+        <img src={logo} alt="The cat reddit logo" height="200px" />
+        <h1>CatReddit</h1>
+        <div className="filter">Hot / New / Top / Etc.</div>
+        <div className="flair-filter">Filter by Flair</div>
       </header>
+      <body>
+        <main>
+          <div className="post-box">
+            <figure>
+              <img />
+            </figure>
+            <p>Posted by u/RedditUser</p>
+            <p><time>12 hours ago</time></p>
+            <h2>Post title</h2>
+            <p>Up vote</p>
+            <p>1222</p>
+            <p>Down vote</p>
+          </div>
+        </main>
+        <aside className="sidebar-left">
+          <section className="rules">
+            <header>
+              <h3>SubReddit Rules</h3>
+            </header>
+            <ol>
+              <li>Rule 1</li>
+              <li>Rule 2</li>
+              <li>Rule 3</li>
+              <li>Rule 4</li>
+              <li>Rule 5</li>
+              <li>Rule 6</li>
+              <li>Rule 7</li>
+              <li>Rule 8</li>
+              <li>Rule 9</li>
+              <li>Rule 10</li>
+              <li>Rule 11</li>
+            </ol>
+          </section>
+          <section className="image-reel">
+            <figure className="cat-image">
+              <p>Upvote</p>
+              <p>Downvote</p>
+              <img />
+            </figure>
+          </section>
+        </aside>
+      </body>
     </div>
   );
 }
