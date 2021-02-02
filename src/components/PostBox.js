12da5b6d1c8de01482ef68909be4cc2006987ecb
Matrim <61281235+MWillis10231@users.noreply.github.com>
2021-02-02 17:08:01 +0100

    still not working on async

--- src/components/PostBox.js
+++ src/components/PostBox.js
@@ -1,25 +1,25 @@
 import cat from '../cat-placeholder.jpg'
-import React from 'react'
+import fetchData from './fetchData'
 
-class PostBox extends React.Component() {
-    render() {
-                (
-                <div className="post-box">
-                    <figure>
-                        <img className="post-image" alt="a cat" src={cat} />
-                    </figure>
-                    <div className="post-content">
-                        <p>postName</p>
-                        <p>Posted by postAuthor</p>
-                        <p><time>postTime</time></p>
-                        <h2>postTitle</h2>
-                        <p>Up vote postUpVotes</p>
-                        <p>postScore</p>
-                        <p>Down vote postDownVotes</p>
-                    </div>
-                </div>
-            )
-    }
+export default function PostBox() {
+    const url = 'https://www.reddit.com/r/cats.json'
+    const { status, data } = fetchData(url);
+
+    return (
+        <div className="post-box">
+                <figure>
+                    <img className="post-image" alt="a cat" src={cat} />
+                </figure>
+            <div className="post-content">
+                <p>{status}</p>
+                <p>{data}</p>
+                <p>Posted by postAuthor</p>
+                <p><time>postTime</time></p>
+                <h2>postTitle</h2>
+                <p>Up vote postUpVotes</p>
+                <p>postScore</p>
+                <p>Down vote postDownVotes</p>
+            </div>
+        </div>
+    )
 }
-
-export default PostBox;
